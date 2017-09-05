8106242eccd622ff31df2ca59faa6f86ef2719e8
hirdbluebird <bluebirdyourdestiny@gmail.com>
2017-09-05 18:32:00 +0300

    router created, ready to push on heroku

--- app.js
+++ app.js
@@ -7,34 +7,11 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')) ;
 
-app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
+app.use(bodyParser());
 
-var todoItems = [
-	{id: 1, desc: 'foo'},
-	{id: 2, desc: 'bar'},
-	{id: 3, desc: 'baz'}
-];
-
-app.get('/', function(req,res){
-	//load data from DB here
-	res.render('index', {
-		title: 'My App',
-		items: todoItems
-	});
-});
-
-app.post('/add', function(req,res){
-	var newItem = req.body.newItem;
-
-	todoItems.push({
-		id: todoItems.length + 1,
-		desc: newItem
-	});
-
-	res.redirect('/');
-});
+app.use(require('./todos'));
 
 app.listen(1337, function(){
 	console.log('ready on port 1337');
