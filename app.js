e7b8cfb8fd89361f7bbc7449fa046c157e7e8f9b
hirdbluebird <bluebirdyourdestiny@gmail.com>
2017-09-07 18:06:31 +0300

    end of tutorial

--- app.js
+++ app.js
@@ -13,10 +13,14 @@ app.use(bodyParser);
 
 app.use(require('./todos'));
 
-app.listen(1337, function(){
+
+
+
+var port = process.env.PORT || 1337;
+
+app.listen(port, function(){
 	console.log('ready on port 1337');
 });
-
 
 /*var http = require('http');
 http.createServer(function(req,res){
